0ca5e89a8e35da10a0a38416ce083f57283f9884
logan_lin <loganlin@aslcn.com.cn>
2023-03-28 16:06:34 +0800

    fix calculation

--- eight_word/产生八字序列.py
+++ eight_word/产生八字序列.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from datetime import datetime, timedelta, date
-from skyfield_get_solar_terms import get_solar_terms
+from skyfield_get_solar_terms import compare_solar_terms, compare_spring_begin
 
 
 class EightWord:
@@ -14,7 +14,7 @@ class EightWord:
         self.hour = hour
         self.minute = minute
         self.second = second
-        self.day_of_year = -1
+        self.datetime_str = f'{self.year}{self.month:0>2d}{self.day:0>2d} {self.hour:0>2d}{self.minute:0>2d}{self.second:0>2d}'
         self.yh = -1
         self.ye = -1
         self.mh = -1
@@ -34,29 +34,53 @@ class EightWord:
         day_of_year = int(the_date.strftime('%j'))
         return day_of_year
 
+    def get_solar_terms_index(self):
+        solar_term_index = compare_solar_terms(self.datetime_str)
+        return solar_term_index
+
+    def test_spring_begin(self):
+        is_after_spring_begin = compare_spring_begin(self.datetime_str)
+        return is_after_spring_begin
+
     def get_year_word(self):
         if self.year < 2000:
             print('年份错误，请输入2000年后的八字')
         else:
-            self.yh = (self.year - 3) % 10 + 10
-            if self.yh > 10:
-                self.yh = self.yh % 10
+            self.yh = (self.year - 3) % 10
+            if self.yh == 0:
+                self.yh = 10
             self.ye = (self.year - 3) % 12
-            print(self.yh, self.ye, end=' ')
+            if not self.test_spring_begin():
+                self.yh = self.yh - 1
+                if self.yh < 1:
+                    self.yh = 10
+                self.ye = self.ye - 1
+                if self.ye < 1:
+                    self.ye = 12
+            # print(self.yh, self.ye, end=' ')
             self.year_word = EightWord.heaven[self.yh] + EightWord.earth[self.ye]
             return self.year_word
 
     def get_month_word(self):
-        datetime_str = str(self.year) + str(self.month) + str(self.day) + ' ' + str(self.hour) + str(self.minute) + str(
-            self.second)
-        solar_term_index = get_solar_terms(datetime_str)
-        self.me = (solar_term_index + 1) // 2 + 4
-        if self.me >= 12:
+        st_idx = self.get_solar_terms_index()
+        self.me = (st_idx + 1) // 2 + 4
+        if self.me > 12:
             self.me = self.me - 12
-        self.mh = (self.yh * 2 % 10 + self.month - 1) % 10
+
+        if st_idx == 18 and self.month == 1:
+            local_year = self.year - 1
+        else:
+            local_year = self.year
+        local_yh = (local_year - 3) % 10
+        if local_yh == 0:
+            local_yh = 10
+        local_me = self.me
+        if self.me == 1:
+            local_me = 13
+        self.mh = (local_yh * 2 + local_me - 2) % 10
         if self.mh == 0:
             self.mh = 10
-        print(self.mh, self.me, end=' ')
+        # print(self.mh, self.me, end=' ')
         self.month_word = EightWord.heaven[self.mh] + EightWord.earth[self.me]
         return self.month_word
 
@@ -65,65 +89,86 @@ class EightWord:
         self.dh = (day_of_year - 5) % 10
         if self.dh == 0:
             self.dh = 10
-        de = (self.day_of_year - 5) % 12 + 12
-        if self.de > 12:
-            de = de % 12
-        print(self.dh, self.de, end=' ')
+        self.de = (day_of_year - 5) % 12
+        if self.de == 0:
+            self.de = 12
+        # print(self.dh, self.de, end=' ')
         self.day_word = EightWord.heaven[self.dh] + EightWord.earth[self.de]
         return self.day_word
 
     def get_hour_word(self):
         if self.hour >= 23:
-            new_date = date(self.year, self.month, self.temp_day) + timedelta(days=1)
+            new_date = date(self.year, self.month, self.day) + timedelta(days=1)
             new_day_of_year = self.get_day_of_year(new_date.year, new_date.month, new_date.day)
-            dh = (new_day_of_year - 5) % 10
+            local_dh = (new_day_of_year - 5) % 10
         else:
             day_of_year = self.get_day_of_year(self.year, self.month, self.day)
-            dh = (day_of_year - 5) % 10
+            local_dh = (day_of_year - 5) % 10
+
         self.he = round((self.hour * 60 + self.minute) / 120) + 1
         if self.he > 12:
             self.he = self.he % 12
-        self.hh = (dh * 2 + self.he - 2) % 10
+        self.hh = (local_dh * 2 + self.he - 2) % 10
         if self.hh == 0:
             self.hh = 10
-        # print(hh, he, end=' ')
+        # print(self.hh, self.he, end=' ')
         self.hour_word = EightWord.heaven[self.hh] + EightWord.earth[self.he]
         return self.hour_word
 
 
 if __name__ == '__main__':
-    y = 2023
-    m = 3
-    mi = 30
-    for d in range(20, 29):
-        for h in range(0, 24):
-            e = EightWord(y, m, d, h, mi)
-            print(f'{y}-{m}-{d} {h}:{mi} :', end=' ', )
-            print(e.get_year_word(), end=' ')
-            print(e.get_month_word(), end=' ')
-            print(e.get_day_word(), end=' ')
-            print(e.get_hour_word())
+    # Test 1
+    # y = 2023
+    # m = 3
+    # mi = 30
+    # for d in range(20, 29):
+    #     for h in range(0, 24):
+    #         e = EightWord(y, m, d, h, mi)
+    #         print(f'{y}-{m}-{d} {h}:{mi} ->', end=' ', )
+    #         print(e.get_year_word(), end=' ')
+    #         print(e.get_month_word(), end=' ')
+    #         print(e.get_day_word(), end=' ')
+    #         print(e.get_hour_word())
 
-    y = 2023
-    h = 12
-    mi = 30
-    for m in range(1, 3):
-        for d in range(0, 29):
-            e = EightWord(y, m, d, h, mi)
-            print(f'{y}-{m}-{d} {h}:{mi} :', end=' ', )
-            print(e.get_year_word(), end=' ')
-            print(e.get_month_word(), end=' ')
-            print(e.get_day_word(), end=' ')
-            print(e.get_hour_word())
+    # Test 2
+    # y = 2023
+    # h = 12
+    # mi = 30
+    # for m in range(1, 3):
+    #     for d in range(1, 29):
+    #         e = EightWord(y, m, d, h, mi)
+    #         print(f'{y}-{m}-{d} {h}:{mi} ->', end=' ', )
+    #         print(e.get_year_word(), end=' ')
+    #         print(e.get_month_word(), end=' ')
+    #         print(e.get_day_word(), end=' ')
+    #         print(e.get_hour_word())
 
-    h = 12
+    # Test 3
+    # h = 12
+    # mi = 30
+    # for y in range(2021, 2024):
+    #     for m in range(1, 13):
+    #         for d in [8, 28]:
+    #             e = EightWord(y, m, d, h, mi)
+    #             print(f'{y}-{m}-{d} {h}:{mi} ->', end=' ', )
+    #             print(e.get_year_word(), end=' ')
+    #             print(e.get_month_word(), end=' ')
+    #             print(e.get_day_word(), end=' ')
+    #             print(e.get_hour_word())
+
+    # test 4
     mi = 30
-    for y in range(2020, 2024):
-        for m in range(1, 13):
-            for d in [8, 28]:
-                e = EightWord(y, m, d, h, mi)
-                print(f'{y}-{m}-{d} {h}:{mi} :', end=' ', )
-                print(e.get_year_word(), end=' ')
-                print(e.get_month_word(), end=' ')
-                print(e.get_day_word(), end=' ')
-                print(e.get_hour_word())
+    for y, m in [(2022, 12), (2023, 1), (2023, 2)]:
+        for d in range(1, 32):
+            for h in [0, 12, 23]:
+                try:
+                    datetime(y, m, d, h, mi)
+                except Exception:
+                    continue
+                else:
+                    e = EightWord(y, m, d, h, mi)
+                    print(f'{y}-{m}-{d} {h}:{mi} ->', end=' ', )
+                    print(e.get_year_word(), end=' ')
+                    print(e.get_month_word(), end=' ')
+                    print(e.get_day_word(), end=' ')
+                    print(e.get_hour_word())
