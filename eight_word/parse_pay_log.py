c7600e3b6094cdac0d5b95998e111b00e5893414
logan_lin <loganlin@aslcn.com.cn>
2023-03-31 17:38:05 +0800

    add parse_pay_log.py

---
+++ eight_word/parse_pay_log.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+import csv
+import re
+
+from MyEightWord import EightWord
+from skyfield_solar_terms import datetime_returns
+
+# 打开文件，写入表头
+f = open('pay_log.txt', 'r', encoding='utf-8')
+pay_log = f.readlines()
+f.close()
+
+t = open('pay_log.csv', 'w', newline='', encoding='utf-8')
+csv_writer = csv.writer(t)
+csv_writer.writerow(['name', 'times', 'year_to_day', 'hour_to_second', 'is_guarantee', 'eight_word'])
+
+for i in pay_log:
+    # 处理祈愿记录
+    pre = i.split(' ')[0]
+    hs = i.split(' ')[1]
+    hs = re.search(r'\d+', hs).group()
+    name_times = pre.split('：')[0]
+    yd = pre.split('：')[1]
+    name = re.search(r'[\u4e00-\u9fa5]+', name_times).group()
+    times = re.search(r'\d+', name_times).group()
+    if '大' in i:
+        guarantee = 1
+    else:
+        guarantee = 0
+
+    # 计算八字
+    e = EightWord(*datetime_returns(yd, hs))
+    eight_word = e.get_year_word() + e.get_month_word() + e.get_day_word() + e.get_hour_word()
+
+    # 写入csv文件
+    csv_writer.writerow([name, times, yd, hs, guarantee, eight_word])
+
+t.close()
